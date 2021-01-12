021d69d8c4ab73aca70380e178652c70bbc4a076
sashagraves <all-exa@ya.ru>
2021-01-12 20:49:59 +0300

    IP written in db fix

--- homework2/server.js
+++ homework2/server.js
@@ -2,26 +2,38 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const port = 8080;
-const dbNames = "names.json"
+const dbNames = "./names.json"
 
 let names = [];
 if (fs.existsSync(dbNames)) {
   names = JSON.parse(fs.readFileSync(dbNames, "utf8"));
-  console.log(">>> names read from file:", names);
-}
+  console.log(names);
+  // names.foreach((item) => console.log("name:", item.name, ", IP: ", item.IP));
+};
 
 const requestHandler = (request, response) => {
   const methodIsValid = request.method === 'POST';
   const queryObject = url.parse(request.url, true).query;
   const headerIsValid = request.headers['iknowyoursecret'] === 'TheOwlsAreNotWhatTheySeem';
+  let clientIP = '';
+  if (request.headers['x-forwarded-for']) {
+    clientIP = request.headers['x-forwarded-for'][0];    
+  } else {
+    clientIP = request.connection.remoteAddress;
+  }
   if (methodIsValid && queryObject.name && headerIsValid) {
-    names.push(queryObject.name);
+    const client = {
+      name: queryObject.name,
+      IP: clientIP,
+    };
+    names.push(client);
     fs.writeFile(dbNames, JSON.stringify(names), (err) => {
       if (err) {
         throw err;
       }
-    })
-    response.end(`Hello, ${names.join(", ")}`);
+    });
+    const messageList = names.map((item) => `Hello, ${item.name} with IP ${item.IP}!`);
+    response.end(messageList.join(" "));
   } else {
     response.end('And do you know my secret?');
   }
